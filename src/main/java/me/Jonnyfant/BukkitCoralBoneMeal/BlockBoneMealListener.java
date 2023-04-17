3b6650577d29fb6baa361c4199d8415389cbd80d
Jonathan Hoffmann <jonathho@axis.com>
2023-04-17 13:18:56 +0200

    wip
    Change-Id: I51ac16460966df819d0695802c14b19b1fb3ff6b

--- src/main/java/me/Jonnyfant/BukkitCoralBoneMeal/BlockBoneMealListener.java
+++ src/main/java/me/Jonnyfant/BukkitCoralBoneMeal/BlockBoneMealListener.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.block.data.type.CoralWallFan;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
@@ -15,61 +16,83 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockFertilizeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlockBoneMealListener implements Listener {
-    private final List<Material> CORAL_BLOCKS = new LinkedList<Material>(Arrays.asList(Material.HORN_CORAL_BLOCK, Material.BUBBLE_CORAL_BLOCK, Material.FIRE_CORAL_BLOCK, Material.BRAIN_CORAL_BLOCK, Material.TUBE_CORAL_BLOCK));
-    private final Map<Material, Material> CORAL_FANS = new HashMap<Material, Material>() {{
-        put(Material.HORN_CORAL_BLOCK, Material.HORN_CORAL_FAN);
-        put(Material.BRAIN_CORAL_BLOCK, Material.BRAIN_CORAL_FAN);
-        put(Material.BUBBLE_CORAL_BLOCK, Material.BUBBLE_CORAL_FAN);
-        put(Material.FIRE_CORAL_BLOCK, Material.FIRE_CORAL_FAN);
-        put(Material.TUBE_CORAL_BLOCK, Material.TUBE_CORAL_FAN);
-    }};
-    private final Map<Material, Material> CORALS = new HashMap<Material, Material>() {{
-        put(Material.HORN_CORAL_BLOCK, Material.HORN_CORAL);
-        put(Material.BRAIN_CORAL_BLOCK, Material.BRAIN_CORAL);
-        put(Material.BUBBLE_CORAL_BLOCK, Material.BUBBLE_CORAL);
-        put(Material.FIRE_CORAL_BLOCK, Material.FIRE_CORAL);
-        put(Material.TUBE_CORAL_BLOCK, Material.TUBE_CORAL);
-    }};
+    private BukkitCoralBoneMeal plug;
+    private final Map<Material, Material> CORAL_FANS = new HashMap<Material, Material>() {
+        {
+            put(Material.HORN_CORAL_BLOCK, Material.HORN_CORAL_FAN);
+            put(Material.BRAIN_CORAL_BLOCK, Material.BRAIN_CORAL_FAN);
+            put(Material.BUBBLE_CORAL_BLOCK, Material.BUBBLE_CORAL_FAN);
+            put(Material.FIRE_CORAL_BLOCK, Material.FIRE_CORAL_FAN);
+            put(Material.TUBE_CORAL_BLOCK, Material.TUBE_CORAL_FAN);
+        }
+    };
+    private final Map<Material, Material> CORALS = new HashMap<Material, Material>() {
+        {
+            put(Material.HORN_CORAL_BLOCK, Material.HORN_CORAL);
+            put(Material.BRAIN_CORAL_BLOCK, Material.BRAIN_CORAL);
+            put(Material.BUBBLE_CORAL_BLOCK, Material.BUBBLE_CORAL);
+            put(Material.FIRE_CORAL_BLOCK, Material.FIRE_CORAL);
+            put(Material.TUBE_CORAL_BLOCK, Material.TUBE_CORAL);
+        }
+    };
+
+    public BlockBoneMealListener (BukkitCoralBoneMeal p)
+    {
+        plug=p;
+    }
+
     @EventHandler
-    public void onBlockBreak(BlockFertilizeEvent event) {
+    public void onBlockBonemeal(BlockFertilizeEvent event) {
         if (applyBoneMeal(event.getBlock()))
             event.setCancelled(true);
     }
 
     public boolean applyBoneMeal(Block b) {
-        boolean changed = false;
-        if (CORAL_BLOCKS.contains(b.getType())) {
-            List<Block> neighbouringBlocks = new LinkedList<Block>();
-            neighbouringBlocks.add(b.getRelative(0, -1, 0));
-            neighbouringBlocks.add(b.getRelative(0, 1, 0));
-            neighbouringBlocks.add(b.getRelative(-1, 0, 0));
-            neighbouringBlocks.add(b.getRelative(1, 0, 0));
-            neighbouringBlocks.add(b.getRelative(0, 0, -1));
-            neighbouringBlocks.add(b.getRelative(0, 0, 1));
-            Random r = new Random();
-
-            for (int i = 0; i < 10; i++) {
-                int ra = r.nextInt(neighbouringBlocks.size());
-                Block newCoral = neighbouringBlocks.get(ra);
-                Bukkit.broadcastMessage("Selected " + ra + "Block: " + newCoral.getType());
-                if (newCoral.getType().equals(Material.WATER)) {
-                    if(ra==1 &&r.nextDouble()>0.5)
-                    {
-                        changed = true;
-                        newCoral.setType(CORALS.get(b.getType()));
-                        break;
-                    }
-                    changed = true;
-                    newCoral.setType(CORAL_FANS.get(b.getType()));
-                    CoralWallFan d = (CoralWallFan) newCoral.getBlockData();
-                    d.setFacing(BlockFace.WEST);
-                    newCoral.setBlockData(d);
-                    break;
-                }
-            }
+        // Not a Coral Block
+        if (!CORALS.containsKey(b.getType())) {
+            return false;
         }
-        return changed;
+        ArrayList<BlockFace> neighbourDirections = new ArrayList<BlockFace>(Arrays.asList(
+                BlockFace.DOWN,
+                BlockFace.UP,
+                BlockFace.EAST,
+                BlockFace.WEST,
+                BlockFace.NORTH,
+                BlockFace.SOUTH));
+        Random r = new Random();
+        Collections.shuffle(neighbourDirections);
+
+        for (int i = 0; i <= neighbourDirections.size(); i++) {
+            BlockFace direction = neighbourDirections.get(i);
+            Block newCoral = b.getRelative(direction);
+
+            //Bukkit.broadcastMessage("Selected " + direction + " Block: " + newCoral.getType());
+
+            // Not Water, next face
+            if (!newCoral.getType().equals(Material.WATER)) {
+                continue;
+            }
+
+            //Chance for coral instead of fan
+            if (direction == BlockFace.UP && r.nextDouble() <= plug.getConfig().getDouble(plug.CFG_NOT_FAN_CHANCE_KEY)) {
+                newCoral.setType(CORALS.get(b.getType()));
+                Waterlogged bd = (Waterlogged) newCoral.getBlockData();
+                bd.setWaterlogged(true);
+                newCoral.setBlockData(bd);
+                return true;
+            }
+
+            // Create coral fan
+            newCoral.setType(CORAL_FANS.get(b.getType()));
+            CoralWallFan d = (CoralWallFan) newCoral.getBlockData();
+            d.setFacing(direction.getOppositeFace());
+            d.setWaterlogged(true);
+            newCoral.setBlockData(d);
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/me/Jonnyfant/BukkitCoralBoneMeal/BukkitCoralBoneMeal.java
+++ src/main/java/me/Jonnyfant/BukkitCoralBoneMeal/BukkitCoralBoneMeal.java
@@ -3,9 +3,18 @@ package me.Jonnyfant.BukkitCoralBoneMeal;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitCoralBoneMeal extends JavaPlugin {
+    public final String CFG_NOT_FAN_CHANCE_KEY = "Chance for a normal coral (Not Fan)";
+    private final double CFG_NOT_FAN_CHANCE_DEFAULT = 0.2;
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new BlockBoneMealListener(), this);
+        initConfig();
+        getServer().getPluginManager().registerEvents(new BlockBoneMealListener(this), this);
     }
 
+    public void initConfig()
+    {
+        getConfig().addDefault(CFG_NOT_FAN_CHANCE_KEY, CFG_NOT_FAN_CHANCE_DEFAULT);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
 }
